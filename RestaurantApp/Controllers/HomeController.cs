052df5bff20682ff5d88899e952982869741e362
Aleksandar94 <aleksandar.stevanovic100@gmail.com>
2023-12-24 23:41:17 +0100

    first commit

---
+++ RestaurantApp/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using RestaurantApp.Models;
+using RestaurantApp.Repositories;
+using RestaurantApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private RestaurantDBEntities objRestaurantDbEntities;
+        public HomeController()
+        {
+            objRestaurantDbEntities = new RestaurantDBEntities();
+        }
+
+
+        public ActionResult Index()
+        {
+            CustomerRepository objCustomerRepository = new CustomerRepository();
+            ItemRepository objItemRepository = new ItemRepository();
+            PaymentTypeRepository objPaymentTypeRepository = new PaymentTypeRepository();
+
+                var objMultipleModels = new Tuple<IEnumerable<SelectListItem>, IEnumerable<SelectListItem>, IEnumerable<SelectListItem>>
+                (objCustomerRepository.GetAllCustomers(),objItemRepository.GetAllItems(),objPaymentTypeRepository.GetAllPaymentType());
+
+
+            return View(objMultipleModels);
+        }
+
+        [HttpGet]
+        public JsonResult getItemUnitPrice(int itemId)
+        {
+            decimal UnitPrice = objRestaurantDbEntities.Items.Single(model => model.ItemId == itemId).ItemPrice;
+            return Json(UnitPrice, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public  JsonResult Index(OrderViewModel objOrderViewModel)
+        {
+
+           /* Debug.WriteLine("Vrednost objOrderViewModel.FinalTotal: " + objOrderViewModel.FinalTotal);*/
+
+
+            OrderRepository objOrderRepository = new OrderRepository();
+            objOrderRepository.AddOrder(objOrderViewModel);
+
+            return Json(" Your order has been Successfully Placed", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
